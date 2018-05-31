b8480155abf14e87059f12a2bddb2e1836e05271
ubuntu <zhouzj@ubuntu14.DHCP>
2018-05-31 22:19:03 +0800

    移除python

--- python/test2/test2.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-if __name__ == "__main__":
-    screen = pygame.display.set_mode((480,890),0,32)
-    background = pygame.image.load("./feiji/background.png").convert()
-
-    while True:
-        screen.blit(background,(0,0))
-        pygame.display.update()
--- python/web/web_helloworld/config.py
+++
@@ -1,6 +0,0 @@
-DEBUG = False
-
-try:
-    from local_settings import *
-except ImportError:
-    pass
